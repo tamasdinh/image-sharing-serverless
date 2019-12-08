05df03f54e8404ce1bd774c450171e069b52bc5d
tamasdinh <tamas.dinh@gmail.com>
2019-12-08 14:19:21 +0100

    feat: added authorization

--- src/lambda/auth/rs256Auth0Authorizer.ts
+++ src/lambda/auth/rs256Auth0Authorizer.ts
@@ -1,20 +1,10 @@
 import { CustomAuthorizerEvent, CustomAuthorizerHandler, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-
-import { verify } from 'jsonwebtoken'
+import {verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 
-const secretId = process.env.AUTH_0_SECRET_ID
-const secretField = process.env.AUTH_0_SECRET_FIELD
-
-const client = new AWS.SecretsManager()
-
-// cache secret if a Lambda instance is reused
-let cachedSecret: string
-
 export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
-
+  
   try {
     const decodedToken = await verifyToken(event.authorizationToken)
     console.log('User was authorized')
@@ -62,21 +52,27 @@ async function verifyToken(authHeader: string): Promise<JwtToken> {
 
   const token = authHeader.split(' ')[1]
 
-  const secret: any = await getSecret()[secretField]
-
-  return verify(token, secret) as JwtToken
+  const cert = `-----BEGIN CERTIFICATE-----
+  MIIDCTCCAfGgAwIBAgIJLsyrAJ1QyOkOMA0GCSqGSIb3DQEBCwUAMCIxIDAeBgNV
+  BAMTF3RkLWRldjIwMTkuZXUuYXV0aDAuY29tMB4XDTE5MTIwNzEzMzMxMloXDTMz
+  MDgxNTEzMzMxMlowIjEgMB4GA1UEAxMXdGQtZGV2MjAxOS5ldS5hdXRoMC5jb20w
+  ggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCdGCEp2SA+J4RpfckIVMSN
+  Q6HQ/PxQVoJxHLSt3pkcUfizWe1u2o9burIYxDR6Riqbm2habUrcKwogfjR2FKM5
+  r68oagzSMLYtH1qaODFI8jSusiOs9f3jPRkKE+OmP5bqmgItV7jZ6/RZj3YbRWUT
+  DGg/7keX/SQw27YBmG7k5RKKenLfU70cJJT3f5B6k1PTQ+272JmnBQNaY3uQl3nq
+  DCPRJ6vubt2Dlv1DhxOwC9CQ6FG9x+9eV1hdzLkjhBh/UAyQ30RaB1td5hzhn48O
+  5C3SPx5NLv6lSYT4143UE01up/rgy8d0zE5N5gNYiucE7G1rdQtXPrq///atFRg5
+  AgMBAAGjQjBAMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFLpwL8eb0DCWBwpE
+  gzOAeJam6MoYMA4GA1UdDwEB/wQEAwIChDANBgkqhkiG9w0BAQsFAAOCAQEAT/nY
+  L4kuDENgq09Dl9OmMdFWjlPShhKCE9og5CngCUtL77YnVmBpcQ6caFriSngTLj6F
+  Q90dPGqpn6m/L4rf509RPKE6kmFPjvYtFGQBbpq25gyzEsJY9Siyd/N67qPglnDz
+  1lGFbAZuX2SuO4WWdisQ9LwEnRewte646Mqx8gop7JgOgY5P16eolg4TddS54UhG
+  WKzradcIUsj4ugs34nRJ0f74SkxtydrtTc3DSJaiJ77DQ61QczrKCW7Kd1o/5sZQ
+  knYhLQ2MO8r7i+67/7LVG9i+vafJrd8oyz0M1jpqSzWSwggKh4M5z4IQDyo49PtK
+  wm2IVfTQ5G0DGWH23w==
+  -----END CERTIFICATE-----`
+  
+  return verify(token, cert, {algorithms: ['RS256']}) as JwtToken
 
   // If there are no exceptions, the request is authorized
-}
-
-async function getSecret () {
-  if (cachedSecret) return cachedSecret
-
-  const data = await client.getSecretValue({
-    SecretId: secretId
-  }).promise()
-
-  cachedSecret = data.SecretString
-
-  return JSON.parse(cachedSecret)
 }
--- src/lambda/http/createGroup.ts
+++ src/lambda/http/createGroup.ts
@@ -1,21 +1,26 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register';
 import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
+import { getUserId } from '../../auth/utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 const groupsTable = process.env.GROUPS_TABLE
 
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> => {
   
   console.log('Processing event:', event)
-
+  
   const itemId = uuid.v4()
   const parsedBody = JSON.parse(event.body)
 
+  const userId = getUserId(event.headers.Authorization.split(' ')[1])
+
   const newItem = {
     id: itemId,
+    userId,
     ...parsedBody
   }
 
